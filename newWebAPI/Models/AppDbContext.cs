5fd6da23b13db924047dfbb6c2df507001653997
WassimMensi <wassim.mensi@ecole-isitech.fr>
2023-12-05 12:32:33 +0100

    Création de BookDb.db

--- newWebAPI/Models/AppDbContext.cs
+++ newWebAPI/Models/AppDbContext.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
 namespace newWebAPI.Models;
 
-public class AppContext : DbContext
+public class AppDbContext : DbContext
 {
-    private const string ConnectionString =@"Server=localhost;Database=BookDb;Trusted_Connection=True;";
-
+    //private const string ConnectionString =@"Server=localhost;Database=BookDb;Trusted_Connection=True;";
+    private const string ConnectionString = "Data Source=BookDb.db";
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(ConnectionString);
